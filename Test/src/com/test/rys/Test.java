3e094f9230ac5c82a551e0270663b3530778e867
ryseok <40780816+ryseok@users.noreply.github.com>
2018-08-25 14:44:22 +0900

    생성자 추가

--- Test/src/com/test/rys/Test.java
+++ Test/src/com/test/rys/Test.java
@@ -1,6 +1,9 @@
 package com.test.rys;
 
 public class Test {
+	public Test() {
+		// TODO Auto-generated constructor stub
+	}
 	public static void main(String[] args) {
 		System.out.println("Hello Git wrold");
 		System.out.println("기능추가");

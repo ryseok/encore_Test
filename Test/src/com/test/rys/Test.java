16d392a7115f89812e83867715bd1b0d0f6a9dc4
ryseok <40780816+ryseok@users.noreply.github.com>
2018-08-25 15:30:13 +0900

    복사복사 함

--- Test/src/com/test/rys/Test.java
+++ Test/src/com/test/rys/Test.java
@@ -3,5 +3,9 @@ package com.test.rys;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hello Git wrold");
+		System.out.println("Hello Git wrold");
+		System.out.println("Hello Git wrold");
+		System.out.println("Hello Git wrold");
+		System.out.println("Hello Git wrold");
 	}
 }

ebe1dc469507f8ebac32acd6f0eb4d79060918d2
ryseok <40780816+ryseok@users.noreply.github.com>
2018-08-25 15:40:27 +0900

    TEst02

--- Test/src/com/test/rys/Test.java
+++ Test/src/com/test/rys/Test.java
@@ -11,6 +11,9 @@ public class Test {
 	
 	public static void main(String[] args) {
 		System.out.println("충돌돌돌돌돌돌돌돌돌돌돌");
+		System.out.println("충돌돌돌돌돌돌돌돌돌돌돌");
+		System.out.println("충돌돌돌돌돌돌돌돌돌돌돌");
+		System.out.println("충돌돌돌돌돌돌돌돌돌돌돌");
 		
 	}
 }

10b4b25633dd7e18d8181120bc4d040eb2a9f95f
ryseok <40780816+ryseok@users.noreply.github.com>
2018-08-25 15:29:38 +0900

    진짜

--- Test/src/com/test/rys/Test.java
+++ Test/src/com/test/rys/Test.java
@@ -10,15 +10,6 @@ public class Test {
 	}
 	
 	public static void main(String[] args) {
-		System.out.println("Hello Git wrold");
-		System.out.println("기능추가");
-		System.out.println("메인페이지");
-		System.out.println("로그인페이지");
-		System.out.println("회원가입페이지");
-		System.out.println("마이페이지");
-		System.out.println("관리자페이지");
-		System.out.println("로그인 휴효성 검사 추가");
-		System.out.println("나 충돌");
 		System.out.println("충돌돌돌돌돌돌돌돌돌돌돌");
 		
 	}

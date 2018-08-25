1538c5e29a379bf9f90b93dbf912097793ec36b4
ryseok <40780816+ryseok@users.noreply.github.com>
2018-08-25 16:07:35 +0900

    no message

--- Test/src/com/test/rys/Test.java
+++ Test/src/com/test/rys/Test.java
@@ -1,27 +1,8 @@
 package com.test.rys;
 
 public class Test {
-	public Test() {
-		// TODO Auto-generated constructor stub
-	}
-	
-	public void hello() {
-		
-	}
-	
+
 	public static void main(String[] args) {
-		System.out.println("???");
-		System.out.println("???");
-		System.out.println("???");
-		System.out.println("???");
-		System.out.println("???");
-		System.out.println("???");
-		System.out.println("???");
-		System.out.println("???");
-		System.out.println("???");
-		System.out.println("???");
-		System.out.println("???");
-		System.out.println("???");
-		
+
 	}
 }

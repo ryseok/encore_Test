eda2d43bbcd8900df7b25232346c41b9925142e7
ryseok <40780816+ryseok@users.noreply.github.com>
2018-08-25 14:50:24 +0900

    hello 메서드 추가

--- Test/src/com/test/rys/Test.java
+++ Test/src/com/test/rys/Test.java
@@ -4,10 +4,16 @@ public class Test {
 	public Test() {
 		// TODO Auto-generated constructor stub
 	}
+	
+	public void hello() {
+		
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Hello Git wrold");
 		System.out.println("기능추가");
 		System.out.println("메인페이지");
 		System.out.println("로그인페이지");
+		
 	}
 }

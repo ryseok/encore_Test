4ab74b1e3b1ca6d0a54ae6684b4927439c45b746
ryseok <40780816+ryseok@users.noreply.github.com>
2018-08-25 16:04:53 +0900

    ??추가

--- Test/src/com/test/rys/Test.java
+++ Test/src/com/test/rys/Test.java
@@ -11,6 +11,17 @@ public class Test {
 	
 	public static void main(String[] args) {
 		System.out.println("???");
+		System.out.println("???");
+		System.out.println("???");
+		System.out.println("???");
+		System.out.println("???");
+		System.out.println("???");
+		System.out.println("???");
+		System.out.println("???");
+		System.out.println("???");
+		System.out.println("???");
+		System.out.println("???");
+		System.out.println("???");
 		
 	}
 }

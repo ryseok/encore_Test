ef7824b687c2a7046db2c23c7c8409ca09883a4c
ryseok <40780816+ryseok@users.noreply.github.com>
2018-08-25 15:47:00 +0900

    main만 있음

--- Test/src/com/test/rys/Test.java
+++ Test/src/com/test/rys/Test.java
@@ -1,16 +1,9 @@
 package com.test.rys;
 
 public class Test {
-	public Test() {
-		// TODO Auto-generated constructor stub
-	}
-	
-	public void hello() {
-		
-	}
-	
+
 	public static void main(String[] args) {
 		System.out.println("충돌 됨?");
-		
+
 	}
 }

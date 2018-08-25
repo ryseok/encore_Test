91f0308b2d89daa7fddb093ade21f1c429671d99
ryseok <40780816+ryseok@users.noreply.github.com>
2018-08-25 16:00:58 +0900

    메인만 있음

--- Test/src/com/test/rys/Test.java
+++ Test/src/com/test/rys/Test.java
@@ -10,12 +10,7 @@ public class Test {
 	}
 	
 	public static void main(String[] args) {
-		System.out.println("충돌 됨?");
-		System.out.println("충돌 됨?");
-		System.out.println("충돌 됨?");
-		System.out.println("충돌 됨?");
-		System.out.println("충돌 됨?");
-		System.out.println("충돌 안됨??");
+
 		
 	}
 }

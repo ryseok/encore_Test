3ae60331b63dcdf0407add66eda7cc821a1d4f7c
ryseok <40780816+ryseok@users.noreply.github.com>
2018-08-25 15:41:35 +0900

    no message

--- Test/src/com/test/rys/Test.java
+++ Test/src/com/test/rys/Test.java
@@ -11,9 +11,6 @@ public class Test {
 	
 	public static void main(String[] args) {
 		System.out.println("충돌돌돌돌돌돌돌돌돌돌돌");
-		System.out.println("충돌돌돌돌돌돌돌돌돌돌돌");
-		System.out.println("충돌돌돌돌돌돌돌돌돌돌돌");
-		System.out.println("충돌돌돌돌돌돌돌돌돌돌돌");
 		
 	}
 }
